bf060b98425e09a975dcab981a13aa5ac390d3d5
Jon Gaspar <jon_gaspar@hotmail.com>
2017-11-01 02:35:42 +0000

    Initial websocket server added

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -10,7 +10,8 @@ class ChatBar extends Component {
 
     this.onContent = this.onContent.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
-
+    
+    this.onClickButton = this.onClickButton.bind(this);
   }
 
   onContent(event) {
@@ -23,7 +24,14 @@ class ChatBar extends Component {
     if (event.charCode === 13) {
       console.log("from ChatBar component:", this.state.content);
       this.props.onNewPost(this.state.content);
+      this.setState({
+        content: ''
+      })
     }
+  }
+
+  onClickButton(event) {
+    console.log(this.state.content);
   }
 
   render() {
@@ -31,6 +39,7 @@ class ChatBar extends Component {
       <footer className="chatbar">
         <input className="chatbar-username" placeholder="Type you name here" onChange={ this.onContent } value={ this.props.currentUser.name } />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={ this.onSubmit } onChange={ this.onContent } value={ this.state.content } />
+        <button onClick={ this.onClickButton }></button>
       </footer>
     );
   }

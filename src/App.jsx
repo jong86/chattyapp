266757c6bb60a829d2f965154ebb72fe3c743549
Jon Gaspar <jon_gaspar@hotmail.com>
2017-11-02 15:58:11 +0000

    Save button for name displayed, is enabled when name changed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentUser: {name: 'Bob'},
+      currentUser: {name: 'Anonymous'},
       messages: [], // messages coming from server are stored here as they arrive
       numUsers: 0
     }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import randomColor from './helpers.js';
+import randomColor from '../scripts/helpers.js';
 
 class ChatBar extends Component {
   constructor(props) {
@@ -7,12 +7,14 @@ class ChatBar extends Component {
     this.state = {
       content: '',
       username: this.props.currentUser.name,
-      prevUsername: undefined,
+      prevUsername: 'Anonymous',
       nameColor: randomColor()
     }
     this.onChangeContent = this.onChangeContent.bind(this);
     this.onChangeUsername = this.onChangeUsername.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+
+    this.isSaveDisabled = this.isSaveDisabled.bind(this);
   }
 
   onChangeContent(event) {
@@ -42,16 +44,26 @@ class ChatBar extends Component {
     }
   }
 
+  isSaveDisabled() {
+    if (this.state.username === this.state.prevUsername) {
+      return <button className='chatbar-savename' disabled>save</button>;
+    } else {
+      return <button className='chatbar-savename'>save</button>;
+    }
+  }
+
   render() {
     console.log('Rendering <ChatBar />');
+    console.log('current username:', this.state.username, 'prevname:', this.state.prevUsername);
     return (
       <footer className='chatbar'>
         <input 
           className='chatbar-username'
-          placeholder='Type you name here'
+          placeholder='Type your name here'
           onChange={ this.onChangeUsername }
           value={ this.state.username }
         />
+        {this.isSaveDisabled()}
         <input
           className='chatbar-message'
           placeholder='Type a message and hit ENTER'
